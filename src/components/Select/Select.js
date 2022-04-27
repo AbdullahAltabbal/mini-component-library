bd3026c0185d53121ed2cf86c97124910ebe4047
altabbal <altabbal@landaumedia.de>
2022-04-27 17:24:37 +0200

    select dropdown done

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,75 @@
-import React from 'react';
-import styled from 'styled-components';
+import { colors } from "@storybook/node-logger";
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativSelect value={value} onChange={onChange}>
+        {children}
+      </NativSelect>
+      <PresentationalBit>
+        {displayedValue}
+        <IconWrapper style={{ "--size": 24 + "px" }}>
+          <Icon id="chevron-down" strokeWidth={1} size={24}></Icon>
+        </IconWrapper>
+      </PresentationalBit>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  width: max-content;
+  position: relative;
+`;
+
+const NativSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+
+  /* Allow the select to span the full height in Safari */
+  -webkit-appearance: none;
+`;
+
+const PresentationalBit = styled.div`
+  border-radius: 8px;
+  font-family: "Roboto", sans-serif;
+  background-color: ${COLORS.transparentGray15};
+  padding: 12px 16px;
+  padding-right: 52px;
+  font-size: 1rem;
+  color: ${COLORS.gray700};
+
+  ${NativSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${NativSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+  width: var(--size);
+  height: var(--size);
+  pointer-events: none;
+  right: 10px;
+`;
 
 export default Select;
